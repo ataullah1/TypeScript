3b4e3123b250e4eb080f65d07b1d9d31b9ca07b3
Md Ataullah <ataullahm100@gmail.com>
2024-11-02 21:27:53 +0600

    TypeScript datatype:

--- Typescript/Practices 1/module1/src/index.ts
+++ Typescript/Practices 1/module1/src/index.ts
@@ -1,3 +1,7 @@
 let hello: string = "Hello i'm Ataullah";
 
 console.log(hello);
+
+const a: number = 123; // Number Data type
+let ab: string = "ataullah"; // String Data type
+const abc: [string, number, string] = ["Hello", 234, "TypeScript"]; // Truple array
