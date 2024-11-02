c09a45055f321c583eeb13b9b4713c9a0b911036
Md Ataullah <ataullahm100@gmail.com>
2024-11-03 00:15:27 +0600

    Function learn

--- Typescript/Practices 1/module1/src/object.ts
+++ Typescript/Practices 1/module1/src/object.ts
@@ -2,8 +2,11 @@ const company: {
   readonly companyName: string;
   employyName: string;
   selary: number;
+  isFounder?: boolean;
+  isRemote: boolean;
 } = {
   companyName: "Snowy tech",
   employyName: "Ataullah",
   selary: 33452,
+  isRemote: true,
 };
