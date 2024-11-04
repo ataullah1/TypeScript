c6bc9297ee2ade546cf9edc87f20836bc0e0069b
Md Ataullah <ataullahm100@gmail.com>
2024-11-05 00:03:19 +0600

    Solved for code run issue

--- Typescript/Practices 2/src/genericType.ts
+++ Typescript/Practices 2/src/genericType.ts
@@ -1,2 +1,9 @@
-let a: string = "Ataullah";
-console.log(a);
+type GenericArray = Array<number>;
+type GenericArraySring = Array<string>;
+type GenericArrayboolean = Array<boolean>;
+
+const rollNo: GenericArray = [4, 234, 6, 7, 8, 9, 8452, 35, 23, 523, 4];
+
+const names: GenericArraySring = ["sadf", "sadf", "fsaf"];
+
+const boolean: GenericArrayboolean = [true, false, true];
